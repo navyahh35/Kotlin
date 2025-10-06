0fc97ee36c1addab40de1666e475237a9dcff7d2
Mohammed Farhan N P <65911037+mohammedfarhannp@users.noreply.github.com>
2025-10-06 19:57:17 +0530

    Create do-while-loop.kt

---
+++ Looping-Statements/do-while-loop.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // var for variables that can be used for iterations (changing variables)
+    // Don't use val for variables that needs to be changed like the 'i' below
+    var i: Int = 0
+    
+    // do while loop
+    do {
+        println(i)
+        i++
+    } while (i < 10)
+}
